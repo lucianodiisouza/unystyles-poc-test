9f2e81c165577733bc2142b4b1f624344cd170c3
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-11 16:52:19 -0300

    Refactor tab layout to use Unistyles theme
    Replaces custom color scheme and tab bar components with theme values from react-native-unistyles. Simplifies tab bar styling and removes unused imports for a more consistent appearance.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,39 @@
-import { Tabs } from 'expo-router';
+import {Tabs} from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {IconSymbol} from '@/components/ui/IconSymbol';
+import {useUnistyles} from 'react-native-unistyles'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const {theme} = useUnistyles()
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarInactiveTintColor: theme.colors.tint,
+                tabBarActiveTintColor: theme.colors.activeTint,
+                sceneStyle: {
+                    backgroundColor: theme.colors.background,
+                },
+                tabBarStyle: {
+                    backgroundColor: theme.colors.foreground
+                }
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({color}) => <IconSymbol size={28} name="house.fill" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="explore"
+                options={{
+                    title: 'Explore',
+                    tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                }}
+            />
+        </Tabs>
+    );
 }
