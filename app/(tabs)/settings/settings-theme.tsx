5d8bc1d0847fc5a5cf8b84d394149c9534e0be1e
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-13 14:09:19 -0300

    Add adaptive theme selection to settings
    Settings screen now allows users to toggle between system and user theme modes using Unistyles adaptive themes. When user mode is selected, options for light and dark themes are displayed.

--- app/(tabs)/settings/settings-theme.tsx
+++ app/(tabs)/settings/settings-theme.tsx
@@ -1,16 +1,43 @@
 import { SettingOptionRadio } from "@/components/SettingOptionRadio";
-import { ScrollView } from "react-native";
-import { StyleSheet } from "react-native-unistyles";
+import { ThemeColor } from "@/components/ThemeColor";
+import { ScrollView, View } from "react-native";
+import {
+  StyleSheet,
+  UnistylesRuntime,
+  useUnistyles,
+} from "react-native-unistyles";
 
 export default function SettingsTheme() {
+  const { rt } = useUnistyles();
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <SettingOptionRadio
         label="System"
-        isSelected={false}
-        onPress={() => {}}
+        isSelected={rt.hasAdaptiveThemes}
+        onPress={() => {
+          if (rt.hasAdaptiveThemes) {
+            return;
+          }
+
+          UnistylesRuntime.setAdaptiveThemes(true);
+        }}
       />
-      <SettingOptionRadio label="User" isSelected={true} onPress={() => {}} />
+      <SettingOptionRadio
+        label="User"
+        isSelected={!rt.hasAdaptiveThemes}
+        onPress={() => {
+          if (rt.hasAdaptiveThemes) {
+            UnistylesRuntime.setAdaptiveThemes(false);
+          }
+        }}
+      />
+      {!rt.hasAdaptiveThemes && (
+        <View style={styles.row}>
+          <ThemeColor label="light" onPress={() => {}} />
+          <ThemeColor label="dark" onPress={() => {}} />
+        </View>
+      )}
     </ScrollView>
   );
 }
@@ -21,5 +48,10 @@ const styles = StyleSheet.create((theme, rt) => ({
     gap: theme.gap(2),
     marginTop: rt.insets.top,
     padding: theme.gap(2),
+  },
+  row: {
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: theme.gap(2),
   },
 }));
