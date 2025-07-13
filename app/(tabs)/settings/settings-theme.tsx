069d7455a20bfc360adf6cb18ebb06ba8dcf879d
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-13 13:53:34 -0300

    Replace theme title with radio options in settings
    Swapped out the ThemedText title for SettingOptionRadio components to allow users to select between 'System' and 'User' theme options. Also added gap styling to improve layout spacing.

--- app/(tabs)/settings/settings-theme.tsx
+++ app/(tabs)/settings/settings-theme.tsx
@@ -1,11 +1,16 @@
-import { ThemedText } from "@/components/ThemedText";
+import { SettingOptionRadio } from "@/components/SettingOptionRadio";
 import { ScrollView } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 
 export default function SettingsTheme() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <ThemedText type="title">Change Theme</ThemedText>
+      <SettingOptionRadio
+        label="System"
+        isSelected={false}
+        onPress={() => {}}
+      />
+      <SettingOptionRadio label="User" isSelected={true} onPress={() => {}} />
     </ScrollView>
   );
 }
@@ -13,6 +18,7 @@ export default function SettingsTheme() {
 const styles = StyleSheet.create((theme, rt) => ({
   container: {
     flex: 1,
+    gap: theme.gap(2),
     marginTop: rt.insets.top,
     padding: theme.gap(2),
   },
