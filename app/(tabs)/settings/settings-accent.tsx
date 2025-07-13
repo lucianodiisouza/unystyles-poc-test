8c83830c0fe88320028236726596070643c8b8ab
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-13 14:36:37 -0300

    Add accent color selection to settings
    Introduces a UI for selecting an accent color from available theme accents. The selected accent is highlighted and can be saved, improving user customization options in the settings.

--- app/(tabs)/settings/settings-accent.tsx
+++ app/(tabs)/settings/settings-accent.tsx
@@ -1,19 +1,81 @@
+import { Button } from "@/components/Button";
 import { ThemedText } from "@/components/ThemedText";
-import { ScrollView } from "react-native";
-import { StyleSheet } from "react-native-unistyles";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Pressable, ScrollView, View } from "react-native";
+import { StyleSheet, useUnistyles } from "react-native-unistyles";
 
 export default function SettingsAccent() {
+  const { theme } = useUnistyles();
+  const allAccents = theme.colors.accents;
+  const [selectedAccent, setSelectedAccent] = useState("banana");
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ThemedText type="title">Change Accent</ThemedText>
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollview}>
+        <View>
+          {Object.entries(allAccents).map(([accentName, accentcolor]) => (
+            <Pressable
+              key={accentName}
+              style={styles.item}
+              onPress={() =>
+                setSelectedAccent(accentName as keyof typeof allAccents)
+              }
+            >
+              <View
+                style={styles.box(accentcolor, accentName === selectedAccent)}
+              />
+              <ThemedText>{accentName}</ThemedText>
+            </Pressable>
+          ))}
+        </View>
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Button
+          label="Save"
+          onPress={() => router.back()}
+          accent={selectedAccent}
+        />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
   container: {
     flex: 1,
-    marginTop: rt.insets.top,
-    padding: theme.gap(2),
   },
+  scrollview: {
+    flex: 1,
+    gap: theme.gap(2),
+    paddingTop: theme.gap(2),
+    paddingHorizontal: theme.gap(2),
+  },
+  row: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    gap: theme.gap(2),
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: theme.gap(2),
+    paddingVertical: theme.gap(2),
+    width: "100%",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.dimmed,
+  },
+  buttonContainer: {
+    marginBottom: rt.insets.bottom,
+    paddingHorizontal: theme.gap(2),
+  },
+  box: (accentColor: string, isSelected: boolean) => ({
+    height: 40,
+    width: 40,
+    backgroundColor: accentColor,
+    borderRadius: 10,
+    borderWidth: isSelected ? 2 : 0,
+    borderColor: theme.colors.tint,
+  }),
 }));
