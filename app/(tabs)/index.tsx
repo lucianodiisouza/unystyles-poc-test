41fb32c8033f39bbdc4f8badcc208ec31cb83d20
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-13 15:09:53 -0300

    Update playlist home to display song list
    Replaces the home screen with a playlist view, rendering songs from the mock playlist and enabling navigation to the player screen for each song. Adjusts layout and styling for improved presentation.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,40 @@
+import { SongTite } from "@/components/SongTitle";
 import { ThemedText } from "@/components/ThemedText";
-import { ScrollView } from "react-native";
+import { playlist } from "@/mocks";
+import { router } from "expo-router";
+import { ScrollView, View } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 
 export default function PlaylistHome() {
   return (
-    <ScrollView style={styles.container}>
-      <ThemedText type="title">Home Screen</ThemedText>
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView style={styles.contentContanier}>
+        <View style={styles.header}>
+          <ThemedText type="title">Playlist</ThemedText>
+        </View>
+
+        {playlist.map((song) => (
+          <SongTite
+            song={song}
+            onPress={() => router.push(`/(tabs)/player/${song.id}`)}
+            key={song.id}
+          />
+        ))}
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
   container: {
-    flex: 1,
-    marginTop: rt.insets.top,
-    padding: theme.gap(2),
+    marginTop: rt.insets.top + theme.gap(3),
+    backgroundColor: theme.colors.background,
+  },
+  contentContanier: {
+    gap: theme.gap(1),
+    paddingHorizontal: theme.gap(2),
+  },
+  header: {
+    paddingBottom: theme.gap(2),
   },
 }));
