ea3fca1091a8c84f4dce7d9e680e60e53470bf54
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-13 12:53:55 -0300

    Refactor settings UI and add SettingTile components
    Replaces links with SettingTile components for theme and accent settings, updates layout to use styled ScrollView and View, and improves appearance section. Integrates Unistyles for adaptive theming and refines styles for better structure.

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,21 +1,44 @@
+import { SettingTile } from "@/components/SettingTile";
 import { ThemedText } from "@/components/ThemedText";
-import { Link } from "expo-router";
-import { ScrollView } from "react-native";
-import { StyleSheet } from "react-native-unistyles";
+import { ScrollView, View } from "react-native";
+import {
+  StyleSheet,
+  UnistylesRuntime,
+  withUnistyles,
+} from "react-native-unistyles";
+
+const StyledScrollview = withUnistyles(ScrollView);
 
 export default function SettingsHome() {
+  const systemTheme = UnistylesRuntime.hasAdaptiveThemes;
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ThemedText type="title">Settings Home</ThemedText>
-      <Link href="/settings/settings-accent">Accent</Link>
-      <Link href="/settings/settings-theme">Theme</Link>
-    </ScrollView>
+    <StyledScrollview contentContainerStyle={styles.scrollview}>
+      <ThemedText type="title">Appearance</ThemedText>
+      <View style={styles.settingsContainer}>
+        <SettingTile
+          settingName="Theme"
+          selectedValue="Light"
+          description={systemTheme ? "System" : "User"}
+          onPress={() => {}}
+        />
+        <SettingTile
+          settingName="App accent"
+          selectedValue="Default"
+          description="Primary app color"
+          onPress={() => {}}
+        />
+      </View>
+    </StyledScrollview>
   );
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
-  container: {
-    flex: 1,
+  scrollview: {
+    marginTop: rt.insets.top + theme.gap(3),
+    backgroundColor: theme.colors.background,
+    paddingHorizontal: theme.gap(2),
+  },
+  settingsContainer: {
     marginTop: rt.insets.top,
     padding: theme.gap(2),
   },
