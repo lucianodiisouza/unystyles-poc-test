f433e019e81c941613b5554ab82b0a518498910d
Luciano dii Souza <lucianodiisouza@hotmail.com>
2025-07-13 12:44:24 -0300

    Add SettingTile component
    Introduces a reusable SettingTile component for displaying a setting name, description, and selected value with pressable functionality. This will help standardize the UI for settings across the app.

---
+++ components/SettingTile.tsx
@@ -0,0 +1,36 @@
+import { Pressable, PressableStateCallbackType, View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
+import { ThemedText } from "./ThemedText";
+
+type SettingTileProps = {
+  settingName: string;
+  selectedValue: string;
+  description: string;
+  onPress(): void;
+};
+
+export const SettingTile: React.FunctionComponent<SettingTileProps> = ({
+  settingName,
+  selectedValue,
+  description,
+  onPress,
+}) => {
+  return (
+    <Pressable onPress={onPress} style={styles.container}>
+      <View>
+        <ThemedText bold>{settingName}</ThemedText>
+        <ThemedText dimmed>{description}</ThemedText>
+      </View>
+      <ThemedText>{selectedValue}</ThemedText>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create(() => ({
+  container: (state: PressableStateCallbackType) => ({
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    opacity: state.pressed ? 0.75 : 1,
+  }),
+}));
